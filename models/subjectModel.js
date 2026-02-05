563b731207827be1f82090a5be6c599be6ee1d30
Muskan yadav <muskanyadav92163@gmail.com>
2026-02-05 10:22:49 +0530

    Add isDeleted field to subject schema

--- models/subjectModel.js
+++ models/subjectModel.js
@@ -4,6 +4,10 @@ const subjectSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
     }
 });
 
